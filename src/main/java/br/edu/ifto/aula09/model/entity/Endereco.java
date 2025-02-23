14e0d9b211d849eaacd11846bcdee6788f0626b7
antonioajsl1975 <antonioajsl1975@gmail.com>
2025-02-23 11:14:10 -0300

    Solucionado problema de relacionamento entre Pessoa e Endereço que não permitia mais de uma venda para um Usuário

--- src/main/java/br/edu/ifto/aula09/model/entity/Endereco.java
+++ src/main/java/br/edu/ifto/aula09/model/entity/Endereco.java
@@ -7,9 +7,10 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
-@Data
+
 public class Endereco {
 
     @Id
@@ -37,14 +38,106 @@ public class Endereco {
     @NotBlank(message = "Número é obrigatório")
     private String numero;
 
-    @ManyToMany
-    @JoinTable(name = "endereco_pessoa",
-            joinColumns = @JoinColumn(name = "endereco_id"),
-            inverseJoinColumns = @JoinColumn(name = "pessoa_id"))
+    @ManyToMany(mappedBy = "enderecos")
     private List<Pessoa> pessoas = new ArrayList<>();
 
-//    @ManyToOne
-//    @JoinColumn(name = "tipo_endereco_id", nullable = false)
-//    private TipoEndereco tipoEndereco;
+    public Endereco() {
+    }
+
+    public Endereco(Long id, String cep, String logradouro, String complemento, String bairro, String cidade, String estado, String numero, List<Pessoa> pessoas) {
+        this.id = id;
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.numero = numero;
+        this.pessoas = pessoas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Endereco endereco = (Endereco) o;
+        return Objects.equals(id, endereco.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
 
